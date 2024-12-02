ba8dd5d44c3a12a2cbe0d0813a11b540b54d3cb1
Raj <raj@klifftechnologies.in>
2024-12-02 17:51:24 +0530

    next.config.js modified

--- next.config.js
+++ next.config.js
@@ -11,6 +11,12 @@ const nextConfig = {
 	images: {
 		remotePatterns: [
 			{
+				protocol: 'https', // Allow images over HTTPS
+				hostname: '**', // Match any domain
+				port: '', // Match any port (default is empty, meaning any port)
+				pathname: '**', // Match any path (image file names, folders, etc.)
+			},
+			{
 				protocol: 'https',
 				hostname: 'images.pexels.com',
 				port: '',
